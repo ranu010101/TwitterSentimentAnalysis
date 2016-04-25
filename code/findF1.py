1bff2113ae842c65d31e6bf9a377cd8f1b4b5f1f
Parag Gangil <paraggangil@gmail.com>
2016-04-25 23:18:16 +0530

    FindF1 change

--- code/findF1.py
+++ code/findF1.py
@@ -1,86 +1,92 @@
 import sys
 if __name__ == '__main__':
     
-    """check arguments"""
+    ####    Check Arguments    ####
     if len(sys.argv)!= 3:                                                   
-        print "Usage :: python findF1.py actualLabel predictedLabel"
+        print "Usage :: python findF1.py actual_Label predicted_Label"
         sys.exit(0)
 
-    predictedLabel=[]
-    actualLabel=[]
+    predicted_Label=[]
+    actual_Label=[]
 
+    ####   
     f=open(sys.argv[1],'r')
     for line in f:
-    	actualLabel.append(line.strip())
+        actual_Label.append(line.strip())
     f.close()
 
     f=open(sys.argv[2],'r')
     for line in f:
-    	predictedLabel.append(line.strip())
+        predicted_Label.append(line.strip())
     f.close()
 
-    countPositiveA=0
-    countNegativeA=0
-    countNeutralA=0
+    #### Initialising variables to zero
+    count_Positive_A=0
+    count_Negative_A=0
+    count_Neutral_A=0
 
-    for i in xrange(len(actualLabel)):
-    	if actualLabel[i]=='positive':
-    		countPositiveA+=1
-    	elif actualLabel[i]=='negative':
-    		countNegativeA+=1
-    	else:
-    		countNeutralA+=1
+    ####    Counting positiveA, negativeA, neutralA    ####
+    for i in xrange(len(actual_Label)):
+        if actual_Label[i]=='positive':
+            count_Positive_A+=1
+        elif actual_Label[i]=='negative':
+            count_Negative_A+=1
+        else:
+            count_Neutral_A+=1
 
-    countPositiveP=0
-    countNegativeP=0
-    countNeutralP=0
+    count_Positive_P=0
+    count_Negative_P=0
+    count_Neutral_P=0
 
-    for i in xrange(len(predictedLabel)):
-    	if predictedLabel[i]=='positive':
-    		countPositiveP+=1
-    	elif predictedLabel[i]=='negative':
-    		countNegativeP+=1
-    	else:
-    		countNeutralP+=1
+    for i in xrange(len(predicted_Label)):
+        if predicted_Label[i]=='positive':
+            count_Positive_P+=1
+        elif predicted_Label[i]=='negative':
+            count_Negative_P+=1
+        else:
+            count_Neutral_P+=1
 
-    truePPositive=0
-    truePNegative=0
-    truePNeutral=0
-    for i in xrange(len(predictedLabel)):
-    	if predictedLabel[i]=='positive':
-    		if actualLabel[i]=='positive':
-    			truePPositive+=1
-    	elif predictedLabel[i]=='negative':
-    		if actualLabel[i]=='negative':
-    			truePNegative+=1
-    	else:
-    		if actualLabel[i]=='neutral':
-    			truePNeutral+=1
+    true_P_Positive=0
+    true_P_Negative=0
+    true_P_Neutral=0
+    for i in xrange(len(predicted_Label)):
+        if predicted_Label[i]=='positive':
+            if actual_Label[i]=='positive':
+                true_P_Positive+=1
+        elif predicted_Label[i]=='negative':
+            if actual_Label[i]=='negative':
+                true_P_Negative+=1
+        else:
+            if actual_Label[i]=='neutral':
+                true_P_Neutral+=1
 
-    precisionPositive = truePPositive/float(countPositiveP)
-    precisionNegative = truePNegative/float(countNegativeP)
-    precisionNeutral = truePNeutral/float(countNeutralP)
+    precision_Positive = true_P_Positive/float(count_Positive_P)
+    precision_Negative = true_P_Negative/float(count_Negative_P)
+    precision_Neutral = true_P_Neutral/float(count_Neutral_P)
 
-    recallPositive = truePPositive/float(countPositiveA)
-    recallNegative = truePNegative/float(countNegativeA)
-    recallNeutral = truePNeutral/float(countNeutralA)
+    recall_Positive = true_P_Positive/float(count_Positive_A)
+    recall_Negative = true_P_Negative/float(count_Negative_A)
+    recall_Neutral = true_P_Neutral/float(count_Neutral_A)
 
-    f1Positive = (2*precisionPositive*recallPositive)/(precisionPositive+recallPositive)
-    f1Negative = (2*precisionNegative*recallNegative)/(precisionNegative+recallNegative)
-    f1Neutral = (2*precisionNeutral*recallNeutral)/(precisionNeutral+recallNeutral)
+    f1_Positive = (2*precision_Positive*recall_Positive)/(precision_Positive+recall_Positive)
+    f1_Negative = (2*precision_Negative*recall_Negative)/(precision_Negative+recall_Negative)
+    f1_Neutral = (2*precision_Neutral*recall_Neutral)/(precision_Neutral+recall_Neutral)
 
+    ####    Print Precision    ####
     print "Precision: Positive, Negative, Neutral"
-    print f1Positive
-    print f1Negative
-    print f1Neutral
+    print f1_Positive
+    print f1_Negative
+    print f1_Neutral
 
+    ####    Print Recall    ####
     print "Recall: Positive, Negative, Neutral"
 
-    print recallPositive
-    print recallNegative
-    print recallNeutral
+    print recall_Positive
+    print recall_Negative
+    print recall_Neutral
 
     print "Average F1 for Positive and Neutral"
 
-    average = (f1Neutral+f1Positive)/2
+    #Print average
+    average = (f1_Neutral+f1_Positive)/2
     print average
